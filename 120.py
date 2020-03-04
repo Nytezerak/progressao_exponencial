275a4fb92c6b69fd656cc5830c240e11dedbba53
Chrystian Ribeiro <60710331+Nytezerak@users.noreply.github.com>
2020-03-03 22:11:45 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,11 @@
+#Autor 1DSI3
+#Chrystian Ribeiro
+#Data 03/03/2020
+#Instituição: ETEC
+
+x=0 #Declaração de comando
+valor - 2 #declaração de variável-valor
+while (x <= 21):
+	valor = valor*valor
+	x = x+1 #Laço de repetição
+print (valor) #Exibe a variável-valor
